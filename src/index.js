ed2afee7245187f636259e3d08de2e357863d15c
Benk513 <79744201+Benk513@users.noreply.github.com>
2024-07-16 23:37:10 +0100

    the first commit , this little project is about developing a fullstack app for POS for a client La Grace , it a shop that sells groceries

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+//import {RouterProvider, createBrowserRouter} from 'react-router-dom'
 import './index.css';
 import App from './App';
+
 import reportWebVitals from './reportWebVitals';
+import Items from './pages/Items';
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <App />,
+//   },
+//   {
+//     path: '/items',
+//     element: <Items/>
+//   }
+// ])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    {/* <RouterProvider router={router} /> */}
     <App />
   </React.StrictMode>
 );
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import DefaultLayout from './../components/DefaultLayout'
+const HomePage = () => {
+  return (
+      <DefaultLayout>
+            <h1>Ben</h1>
+       </DefaultLayout>
+  )
+}
+
+export default HomePage
